8c52fd14646093bb7a98f800bcfeb9f4c7ae6a26
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-04-15 22:30:43 +0200

    Update mill

--- flake.nix
+++ flake.nix
@@ -10,40 +10,17 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        mill = pkgs.mill.overrideAttrs (old: rec {
-          version = "1.0.5";
-          src = pkgs.fetchurl {
-            url = "https://repo1.maven.org/maven2/com/lihaoyi/mill-dist-native-linux-amd64/${version}/mill-dist-native-linux-amd64-${version}.exe";
-            hash = "sha256-xxQMCJ659b/RrSzGKYvXdTcXS9F3TZRGTAbcAc7yF3o=";
-          };
-          buildInputs = [ pkgs.zlib ];
-          nativeBuildInputs = [
-            pkgs.makeWrapper
-          ]
-          ++ pkgs.lib.optional pkgs.stdenvNoCC.isLinux pkgs.autoPatchelfHook;
-
-          installPhase = ''
-            runHook preInstall
-
-            install -Dm 555 $src $out/bin/.mill-wrapped
-            # can't use wrapProgram because it sets --argv0
-            makeWrapper $out/bin/.mill-wrapped $out/bin/mill \
-              --prefix PATH : "${pkgs.jre}/bin" \
-              --set-default JAVA_HOME "${pkgs.jre}"
-
-            runHook postInstall
-          '';
-          doInstallCheck = false;
-        });
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = [
-            mill
-            pkgs.scalafmt
+            pkgs.mill
             pkgs.tailwindcss_4
           ];
-          shellHook = "mill --bsp-install";
+          shellHook = ''
+            export JAVA_HOME="${pkgs.jdk}";
+            mill --bsp-install
+          '';
         };
       }
     );

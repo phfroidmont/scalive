6b4af8ba8526b710fa67d3683dab727b8d779f9d
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-04-26 04:41:19 +0200

    test(liveview): cover nested component updates

--- scalive/test/src/scalive/SocketSpec.scala
+++ scalive/test/src/scalive/SocketSpec.scala
@@ -661,6 +661,60 @@ object SocketSpec extends ZIOSpecDefault:
 
         assertTrue(componentUpdated)
     },
+    test("sendUpdate applies typed props to a nested live component") {
+      class LabelComponent extends LiveComponent[String, Unit, String]:
+        def mount(props: String)                            = ZIO.succeed(props)
+        override def update(props: String, model: String)   = ZIO.succeed(props)
+        def handleMessage(model: String)                    = _ => ZIO.succeed(model)
+        def render(model: String, self: ComponentRef[Unit]) =
+          div(model)
+
+      object ParentComponent extends LiveComponent[Unit, Unit, Unit]:
+        def mount(props: Unit)                    = ZIO.unit
+        def handleMessage(model: Unit)            = _ => ZIO.succeed(model)
+        def render(model: Unit, self: ComponentRef[Unit]) =
+          div(liveComponent(LabelComponent(), id = "label", props = "initial"))
+
+      val lv = new LiveView[Unit, Unit]:
+        def mount                      = ZIO.unit
+        def handleMessage(model: Unit) =
+          _ => LiveContext.sendUpdate[LabelComponent]("label", "updated").as(model)
+        def render(model: Unit): HtmlElement[Unit] =
+          div(
+            button(phx.onClick(()), "update"),
+            liveComponent(ParentComponent, id = "parent", props = ())
+          )
+        def subscriptions(model: Unit) = ZStream.empty
+
+      val event: Payload.Event = Payload.Event(
+        `type` = "click",
+        event = BindingId.attrBindingId(Vector("root:div", "tag:0:button"), 0),
+        value = Json.Obj.empty
+      )
+
+      for
+        socket     <- Socket.start("id", "token", lv, LiveContext(staticChanged = false), meta)
+        replyFiber <- socket.outbox.drop(1).runHead.fork
+        _          <- socket.inbox.offer(event -> meta)
+        reply      <- replyFiber.join.some
+      yield
+        val componentUpdated = reply._1 match
+          case Payload.Reply(
+                ReplyStatus.Ok,
+                LiveResponse.Diff(Diff.Tag(_, _, _, _, _, components, _, _))
+              ) =>
+            components.get(2).exists {
+              case Diff.Tag(_, dynamic, _, _, _, _, _, _) =>
+                dynamic.exists {
+                  case Diff.Dynamic(_, Diff.Value("updated")) => true
+                  case _                                      => false
+                }
+              case _ => false
+            }
+          case _ => false
+
+        assertTrue(componentUpdated)
+    },
     test("live component streams with the same name are scoped per component") {
       object StreamComponent extends LiveComponent[String, Unit, LiveStream[String]]:
         private val Items = LiveStreamDef.byId[String, String]("items")(identity)
